a283f4f2f9ed7d4cf137ffd27b0216f1936251b2
Steven Straatemans <steven@straatemans.nl>
2021-01-01 13:11:58 +0100

    adding storybook setup

--- src/ReadMore.stories.tsx
+++ src/ReadMore.stories.tsx
@@ -3,8 +3,6 @@ import ReadMore from './ReadMore';
 import { storiesOf, action } from '@storybook/react';
 import { Wrapper } from './storybook';
 
-import { Meta } from '@storybook/react/types-6-0';
-
 const Text = `
         Lorem ipsum dolor sit amet, consectetur adippscing elit. Duis eu neque lacus. Mauris
         scelerisque sed arcu vel pharetra. Aenean nec nulla sed nulla viverra cursus at et lacus.
@@ -22,19 +20,19 @@ const Text = `
         consectetur ipsum nec sem eleifend ultricies.`;
 
 storiesOf('Read more', module)
-    .addDecorator((storyFn) => <Wrapper>{storyFn()}</Wrapper>)
-    .add('max characters', () => (
-        <ReadMore maxCharacters={50} readMoreLabel="read more" readLessLabel="read less">
-            {Text}
-        </ReadMore>
-    ))
-    .add('max words', () => (
-        <ReadMore maxWords={20} readMoreLabel="read more" readLessLabel="read less">
-            {Text}
-        </ReadMore>
-    ))
-    .add('max lines', () => (
-        <ReadMore maxLines={2} readMoreLabel="read more" readLessLabel="read less">
-            {Text}
-        </ReadMore>
-    ));
+  .addDecorator((storyFn) => <Wrapper>{storyFn()}</Wrapper>)
+  .add('max characters', () => (
+    <ReadMore maxCharacters={50} readMoreLabel='read more' readLessLabel='read less'>
+      {Text}
+    </ReadMore>
+  ))
+  .add('max words', () => (
+    <ReadMore maxWords={20} readMoreLabel='read more' readLessLabel='read less'>
+      {Text}
+    </ReadMore>
+  ))
+  .add('max lines', () => (
+    <ReadMore maxLines={2} readMoreLabel='read more' readLessLabel='read less'>
+      {Text}
+    </ReadMore>
+  ));
