6eb57b41dc958051bdaaa7af948e6c1a89d92f31
Steven Straatemans <steven@straatemans.nl>
2021-01-04 23:28:37 +0100

    no message

--- src/ReadMore.stories.tsx
+++ src/ReadMore.stories.tsx
@@ -2,27 +2,12 @@ import React from 'react';
 import { Meta } from '@storybook/react';
 import ReadMore from './ReadMore';
 import './storybook.scss';
+import { Text } from './Text';
 
 interface iStory<P = unknown> extends React.VFC<P> {
     args: { [key: string]: string | number };
     storyName: string;
 }
-
-const Text = `
-        Lorem ipsum dolor sit amet, consectetur adippscing elit. Duis eu neque lacus. Mauris
-        scelerisque sed arcu vel pharetra. Aenean nec nulla sed nulla viverra cursus at et lacus.
-        Etiam accumsan turpis ac consequat sodales. In sollicitudin egestas arcu, et vulputate nunc
-        semper in. Praesent interdum odio ac tempor feugiat. Integer id sapien a enim iaculis
-        fringilla sed ac lacus. Vivamus odio enim, faucibus vitae nibh malesuada, semper dapibus
-        massa. Fusce ligula lorem, dictum sit amet elit sit amet, tempor feugiat nulla. Vestibulum
-        non luctus dolor. Vestibulum consectetur ipsum nec sem eleifend ultricies. Lorem ipsum dolor
-        sit amet, consectetur adippscing elit. Duis eu neque lacus. Mauris scelerisque sed arcu vel
-        pharetra. Aenean nec nulla sed nulla viverra cursus at et lacus. Etiam accumsan turpis ac
-        consequat sodales. In sollicitudin egestas arcu, et vulputate nunc semper in. Praesent
-        interdum odio ac tempor feugiat. Integer id sapien a enim iaculis fringilla sed ac lacus.
-        Vivamus odio enim, faucibus vitae nibh malesuada, semper dapibus massa. Fusce ligula lorem,
-        dictum sit amet elit sit amet, tempor feugiat nulla. Vestibulum non luctus dolor. Vestibulum
-        consectetur ipsum nec sem eleifend ultricies.`;
 
 const templateDecorator = (Story) => (
     <div className="wrapper">
