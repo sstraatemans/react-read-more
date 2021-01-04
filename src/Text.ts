55b4bbfa59e70d8262ed698b591a958985a6a63b
Steven Straatemans <steven@straatemans.nl>
2021-01-04 23:28:59 +0100

    text

---
+++ src/Text.ts
@@ -0,0 +1,15 @@
+export const Text = `
+        Lorem ipsum dolor sit amet, consectetur adippscing elit. Duis eu neque lacus. Mauris
+        scelerisque sed arcu vel pharetra. Aenean nec nulla sed nulla viverra cursus at et lacus.
+        Etiam accumsan turpis ac consequat sodales. In sollicitudin egestas arcu, et vulputate nunc
+        semper in. Praesent interdum odio ac tempor feugiat. Integer id sapien a enim iaculis
+        fringilla sed ac lacus. Vivamus odio enim, faucibus vitae nibh malesuada, semper dapibus
+        massa. Fusce ligula lorem, dictum sit amet elit sit amet, tempor feugiat nulla. Vestibulum
+        non luctus dolor. Vestibulum consectetur ipsum nec sem eleifend ultricies. Lorem ipsum dolor
+        sit amet, consectetur adippscing elit. Duis eu neque lacus. Mauris scelerisque sed arcu vel
+        pharetra. Aenean nec nulla sed nulla viverra cursus at et lacus. Etiam accumsan turpis ac
+        consequat sodales. In sollicitudin egestas arcu, et vulputate nunc semper in. Praesent
+        interdum odio ac tempor feugiat. Integer id sapien a enim iaculis fringilla sed ac lacus.
+        Vivamus odio enim, faucibus vitae nibh malesuada, semper dapibus massa. Fusce ligula lorem,
+        dictum sit amet elit sit amet, tempor feugiat nulla. Vestibulum non luctus dolor. Vestibulum
+        consectetur ipsum nec sem eleifend ultricies.`;
